52a654b6971ea812caeb13829962a4f6c861a7bf
Luiz Ricardo <luu.izz@hotmail.com>
2025-06-10 20:07:09 -0300

    feat: criado componente de Resumo e feito fetch nos dados

--- src/pages/Resumo.tsx
+++ src/pages/Resumo.tsx
@@ -1,5 +1,45 @@
-import React from 'react';
+import { useData } from '@/context/DataContext';
 
 export default function Resumo() {
-  return <div>Resumo</div>;
+  const { data } = useData();
+
+  if (data === null) return null;
+
+  return (
+    <section>
+      <div className="resumo flex mb">
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .filter((item) => item.status !== 'falha')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((item) => item.status === 'pago')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((item) => item.status === 'processando')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+      </div>
+
+      <div className="box mb">Gráficos</div>
+    </section>
+  );
 }
