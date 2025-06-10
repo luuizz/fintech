09fd3dbd30dcaa3b4bb47d84789b2d41952f825e
Luiz Ricardo <luu.izz@hotmail.com>
2025-06-10 20:09:31 -0300

    fix: refinado componentes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import styles from './styles.module.scss';
 import { useData } from '@/context/DataContext';
 import DateRange from './DateRange';
+import Meses from './Meses';
 
 export default function Header() {
+  const [title, setTitle] = React.useState('Resumo');
   return (
-    <header className={styles.header}>
-      <DateRange />
+    <header className={`${styles.header} mb`}>
+      <div className="daterange mb">
+        <DateRange />
+        <h1 className="box bg-3">{title}</h1>
+      </div>
+      <Meses />
     </header>
   );
 }
